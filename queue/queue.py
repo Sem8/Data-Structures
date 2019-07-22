1a1abd6ae3dc780aef89f2444fd8b0b490a662d5
Sem8 <mssemmi8@gmail.com>
2019-07-22 17:28:36 -0400

    all queue tests pass but with python built in list methods

--- queue/queue.py
+++ queue/queue.py
@@ -5,21 +5,30 @@ class Queue:
     # use to store queue elements? - Array
     self.storage = []
 
-  def enqueue(self, item):
-    pass
+  def enqueue(self, item):    
+    self.storage.append(item)
+    # pass
   
   def dequeue(self):
-    pass
+    if not self.storage:
+      return None
+    else:
+      return self.storage.pop(0)
+    # pass
 
   def len(self):
-    count = 0
-    current = self.storage
-
-    if not self.storage:
-      return 0
-    else:
-      while current:
-        count += 0
-        
-    return count
+    return len(self.storage)
     # pass
+
+  # def len(self):
+  #   count = 0
+  #   current = self.storage
+
+  #   if not self.storage:
+  #     return 0
+  #   else:
+  #     while current:
+  #       count += 0
+
+  #   return count
+  #   # pass
