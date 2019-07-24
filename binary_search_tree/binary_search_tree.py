f344027176101976d63dc882e4e752ada707a6f9
Sem8 <mssemmi8@gmail.com>
2019-07-24 18:58:59 -0400

    contains method for binary search tree written

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,10 +5,43 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    else:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
 
   def contains(self, target):
-    pass
+    if target > self.value and self.right:
+      return self.right.contains(target)    
+    elif target < self.value and self.left:
+      return self.left.contains(target)
+    elif target == self.value:
+      return True
+    else:
+      return False
+
+  # def contains(self, target):
+  #   if target > self.value and self.right:
+  #     is_found = self.contains(target)
+  #     if is_found:
+  #       return True
+  #     return False
+  #   elif target < self.value and self.left:
+  #     is_found = self.contains(target)
+  #     if is_found:
+  #       return True
+  #     return False
+  #   if target == self.value:
+  #     return True
+  #   else:
+  #     return None
+
 
   def get_max(self):
     pass
