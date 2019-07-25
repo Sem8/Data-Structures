e352968f2ef5937e74f9e5372e025c40f0aab5e3
Sem8 <mssemmi8@gmail.com>
2019-07-24 20:09:29 -0400

    get max method of binary search tree finally passes

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -16,35 +16,54 @@ class BinarySearchTree:
       else:
         self.right.insert(value)
 
-  def contains(self, target):
-    if target > self.value and self.right:
-      return self.right.contains(target)    
-    elif target < self.value and self.left:
-      return self.left.contains(target)
-    elif target == self.value:
-      return True
-    else:
-      return False
-
+# contains method with cleaner code:
   # def contains(self, target):
   #   if target > self.value and self.right:
-  #     is_found = self.contains(target)
-  #     if is_found:
-  #       return True
-  #     return False
+  #     return self.right.contains(target)    
   #   elif target < self.value and self.left:
-  #     is_found = self.contains(target)
-  #     if is_found:
-  #       return True
-  #     return False
-  #   if target == self.value:
+  #     return self.left.contains(target)
+  #   elif target == self.value:
   #     return True
   #   else:
-  #     return None
+  #     return False
 
+# contains method that makes more sense to me:
+  def contains(self, target):
+    if target > self.value and self.right:
+      is_found = self.right.contains(target)
+      if is_found:
+        return True
+      return False
+    elif target < self.value and self.left:
+      is_found = self.left.contains(target)
+      if is_found:
+        return True
+      return False
+    if target == self.value:
+      return True
+    else:
+      return None
 
   def get_max(self):
-    pass
+    max_value = self.value
+
+    if self.right:      
+      max_value = self.right.get_max()
+          
+    return max_value
+
+
+  # def get_max(self):
+  #   max_value = self.value
+  #   if not self.right:
+  #     if self.left:
+  #       self.left.get_max()
+  #       max_value = self.left.value
+  #   elif self.right:
+  #     self.right.get_max()
+  #     max_value = self.right.value
+  #   return max_value
+    
 
   def for_each(self, cb):
     pass
